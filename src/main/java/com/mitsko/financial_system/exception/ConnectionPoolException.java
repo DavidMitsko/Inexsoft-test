5bb6fab70807fb76419ce7e25b7e66f7a79d1350
DavidMitsko <david.mitko@mail.ru>
2023-10-07 19:20:52 +0300

    Create connection pool to db

---
+++ src/main/java/com/mitsko/financial_system/exception/ConnectionPoolException.java
@@ -0,0 +1,20 @@
+package com.mitsko.financial_system.exception;
+
+public class ConnectionPoolException extends Exception {
+
+    public ConnectionPoolException() {
+    }
+
+    public ConnectionPoolException(String message) {
+        super(message);
+    }
+
+    public ConnectionPoolException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConnectionPoolException(Throwable cause) {
+        super(cause);
+    }
+
+}
