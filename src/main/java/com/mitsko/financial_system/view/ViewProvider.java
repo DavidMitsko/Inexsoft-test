fba5a10d18f3296f368a25e3a367874640900f90
DavidMitsko <david.mitko@mail.ru>
2023-10-08 02:34:03 +0300

    Add view provider

---
+++ src/main/java/com/mitsko/financial_system/view/ViewProvider.java
@@ -0,0 +1,45 @@
+package com.mitsko.financial_system.view;
+
+import com.mitsko.financial_system.domain.enums.CommandName;
+import com.mitsko.financial_system.exception.ValidationException;
+import com.mitsko.financial_system.view.impl.BankInfoViewImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ViewProvider {
+
+    private static final Logger logger = LoggerFactory.getLogger(ViewProvider.class);
+
+    private static final ViewProvider instance = new ViewProvider();
+
+    private final Map<CommandName, View> repository = new HashMap<>();
+
+    public static ViewProvider getInstance() {
+        return instance;
+    }
+
+    public ViewProvider(){
+        repository.put(CommandName.GET_ALL_BANKS, new BankInfoViewImpl());
+    }
+
+    public View getView(String name) throws ValidationException {
+        CommandName commandName;
+        View view;
+
+        try {
+            commandName = Arrays.stream(CommandName.values()).filter(c -> c.getCommand().equals(name))
+                    .findFirst().orElseThrow(() -> new ValidationException("Wrong command"));
+            view = repository.get(commandName);
+        } catch (IllegalArgumentException | NullPointerException | ValidationException e) {
+            logger.error(e.getMessage());
+            throw new ValidationException(e);
+        }
+
+        return view;
+    }
+
+}
