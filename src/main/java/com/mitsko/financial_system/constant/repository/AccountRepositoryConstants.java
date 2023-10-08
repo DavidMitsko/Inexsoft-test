1643a056cb4cf708864dc88f16be635801cb7d6b
DavidMitsko <david.mitko@mail.ru>
2023-10-08 05:08:25 +0300

    Refactoring

--- src/main/java/com/mitsko/financial_system/constant/repository/AccountRepositoryConstants.java
+++ src/main/java/com/mitsko/financial_system/constant/repository/AccountRepositoryConstants.java
@@ -4,7 +4,8 @@ public class AccountRepositoryConstants {
 
     public static final String CREATE_NEW_ACCOUNT = "INSERT INTO account(uuid, balance, client_uuid, bank_uuid, currency)" +
             " VALUES(?,?,?,?,?)";
-    public static final String GET_ACCOUNT_BY_UUID = "SELECT * FROM account WHERE uuid = ?";
+    public static final String GET_ACCOUNT_BY_UUID = "SELECT uuid, balance, client_uuid, bank_uuid, currency " +
+            "FROM account WHERE uuid = ?";
     public static final String GET_ALL_ACCOUNTS_BY_CLIENT = "SELECT uuid, balance, client_uuid, bank_uuid, currency " +
             "FROM account WHERE client_uuid = ?";
     public static final String UPDATE_ACCOUNT_BALANCE_BY_CLIENT = "UPDATE account SET balance = ? WHERE client_uuid = ?";
--- src/main/java/com/mitsko/financial_system/constant/repository/BankRepositoryConstants.java
+++ src/main/java/com/mitsko/financial_system/constant/repository/BankRepositoryConstants.java
@@ -4,7 +4,8 @@ public class BankRepositoryConstants {
 
     public static final String CREATE_NEW_BANK = "INSERT INTO bank(uuid, name, individuals_commission, legal_entities_commission)" +
             " VALUES(?,?,?,?)";
-    public static final String GET_BANK_BY_UUID = "SELECT * FROM bank WHERE uuid = ?";
+    public static final String GET_BANK_BY_UUID = "SELECT uuid, name, individuals_commission, legal_entities_commission " +
+            "FROM bank WHERE uuid = ?";
     public static final String GET_ALL_BANKS = "SELECT * FROM bank";
     public static final String UPDATE_BANK_BY_UUID = "UPDATE bank SET name = ?, individuals_commission = ?, " +
             "legal_entities_commission = ? WHERE uuid = ?";
